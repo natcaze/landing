11642b72a84c5c0ba27e11807733b53d36cdd323
Nathalie Cazemajou <nat.caze@gmail.com>
2022-12-16 23:33:24 +0000

    pages updated

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import logo from "../src/assets/logo.png";
 import Page1 from "./pages/Page1";  
 import Page2 from "./pages/Page2";  
 import Page3 from "./pages/Page3";  
+import WatchNow from "./pages/WatchNow";  
 
 
 function App() {
@@ -18,9 +19,10 @@ function App() {
       </div>
 
       <Routes>
-        <Route path="/page1" element={<Page1 />} />
+        <Route path="/" element={<Page1 />} />
         <Route path="/page2" element={<Page2 />} />
         <Route path="/page3" element={<Page3 />} />
+        <Route path="/watchnow" element={<WatchNow />} />
       </Routes>
 
       <Footer />
---
+++ src/pages/WatchNow.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import watchMovies from "../assets/watchMovies.png";
+
+function WatchNow() {
+  return (
+    <div>
+      <img src={watchMovies} alt="watchMovies" />
+    </div>
+  );
+}
+
+export default WatchNow;
