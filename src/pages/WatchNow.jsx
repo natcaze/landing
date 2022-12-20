0f0e92f19055a83f115296b16425230f2c93d0f9
Nathalie Cazemajou <nat.caze@gmail.com>
2022-12-20 14:01:05 +0000

    styling watch now updated

--- src/pages/WatchNow.jsx
+++ src/pages/WatchNow.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 
-import watchMovies from "../assets/watchMovies.png";
 
 function WatchNow() {
   return (
     <div className="watch">
-      <img src={watchMovies} alt="watchMovies" />
+
+      <h4>WATCH NEW MOVIES FOR FREE</h4>
+      <p className="watch-new">
+       Watch any movies online for free without ads!
+      </p>
+
+      <p className="watch-new">
+        Have fun watching your favourite movies!
+      </p>
+
+      <div className="button">
+        <button>WATCH HERE!</button>
+      </div>
+      
     </div>
   );
 }
